74d585b4f811483a297153dbb242ddaeec57cf06
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2026-05-22 11:40:57 +0530

    adding basic setup of project and  commit by priyanshu

--- moxcrm/src/app/home/page.jsx
+++ moxcrm/src/app/home/page.jsx
@@ -1,6 +1,8 @@
 function page() {
+
   return (
-    <div>page</div>
+    
+    <div className="text-center h-screen flex items-center justify-center font-bold text-3xl">welcome to Moxcrm Deal page</div>
   )
 }
 
