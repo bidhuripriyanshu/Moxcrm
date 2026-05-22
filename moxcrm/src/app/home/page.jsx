9bfbd442674426b4642c4f63170b15ca56bf37d5
Priyanshu bidhuri <156426411+bidhuripriyanshu@users.noreply.github.com>
2026-05-22 13:45:56 +0530

    merge

--- moxcrm/src/app/home/page.jsx
+++ moxcrm/src/app/home/page.jsx
@@ -4,7 +4,8 @@ function page() {
   return (
     
     <div className="text-center h-screen flex items-center justify-center font-bold text-3xl">welcome to Moxcrm Deal page</div>
-  )
+    
+    )
 }
 
 export default page
